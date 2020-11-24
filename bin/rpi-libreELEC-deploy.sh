833e9706e9baf2f8f0b9741cb2e29847bc6a178a
Claudiu Soroiu <csoroiu@gmail.com>
2020-11-24 09:29:18 +0200

    version for libreelec is now configurable

--- bin/rpi-libreELEC-deploy.sh
+++ bin/rpi-libreELEC-deploy.sh
@@ -3,22 +3,25 @@ readonly PROGNAME=$(basename $0)
 readonly PROGDIR="$(dirname -- "$(readlink -f -- "$0")")"
 readonly ARGS="$@"
 
+LIBREELEC_VERSION=${LIBREELEC_VERSION:=9.2.6}
+
 if [[ $# -ne 2 ]]; then
     echo "Invalid arguments provided."
     echo "First argument needs to be the device where to write the image"
     echo "Second argument needs to be the distro name:"
     echo " RPi2: for rpi-2 and rpi-3"
     echo " RPi4: for rpi-4"
+    echo "The LIBREELEC_VERSION variable controls the version to deploy."
     exit 1
 fi
 
 device="$1"
 distro="$2"
 
-"${PROGDIR}/rpi-libreELEC-9.2.3-get.sh" ${distro}
+"${PROGDIR}/rpi-libreELEC-get.sh" ${distro}
 
 #real file_name is known only after download
-file_name="LibreELEC-${distro}.arm-9.2.3.img.gz"
+file_name="LibreELEC-${distro}.arm-${LIBREELEC_VERSION}.img.gz"
 
 #writing image to disk
 sudo "${PROGDIR}/write-image.sh" "${device}" "${file_name}"
--- bin/rpi-libreELEC-get.sh
+++ bin/rpi-libreELEC-get.sh
@@ -5,16 +5,19 @@ readonly ARGS="$@"
 
 . $PROGDIR/download-functions.sh --source-only
 
+LIBREELEC_VERSION=${LIBREELEC_VERSION:=9.2.6}
+
 if [[ $# -ne 1 ]]; then
     echo "Invalid arguments provided. Needs to receive the distro name:"
     echo " RPi2: for rpi-2 and rpi-3"
     echo " RPi4: for rpi-4"
+    echo "The LIBREELEC_VERSION variable controls the version to deploy."
     exit 1
 fi
 
 distro="$1"
 
-url="http://releases.libreelec.tv/LibreELEC-${distro}.arm-9.2.3.img.gz"
+url="http://releases.libreelec.tv/LibreELEC-${distro}.arm-${LIBREELEC_VERSION}.img.gz"
 file_name="$(get_file_name_from_url ${url})"
 image_sha256="${file_name}.sha256"
 
