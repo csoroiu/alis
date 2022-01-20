d0b84a38caa46e7a03ce32e8168e96cb365eca68
Claudiu Soroiu <csoroiu@gmail.com>
2022-01-20 13:20:39 +0200

    k3s upgrade to v1.23.1+k3s2

---
+++ bin/rpi-ubuntucore-deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+readonly PROGNAME=$(basename "$0")
+readonly PROGDIR="$(dirname -- "$(readlink -f -- "$0")")"
+readonly ARGS=("$@")
+
+UBUNTU_VERSION=${UBUNTU_VERSION:=21.10}
+
+if [[ $# -ne 2 ]]; then
+    echo "Invalid arguments provided."
+    echo "First argument needs to be the device where to write the image"
+    echo "Second argument needs to be the distro name:"
+    echo " armhf: for rpi-2,3,4"
+    echo " arm64: for rpi-3,4"
+    echo "The UBUNTU_VERSION variable controls the version to deploy."
+    exit 1
+fi
+
+if [[ "$EUID" -eq 0 ]]; then
+  echo "Do not run this script as root."
+  exit
+fi
+
+device="$1"
+distro="$2"
+
+
+#downloading file
+"${PROGDIR}/rpi-ubuntu-get.sh" "${distro}"
+
+#real file_name is known only after download
+file_name="ubuntu-${UBUNTU_VERSION}-preinstalled-server-${distro}+raspi.img.xz"
+
+#writing image to disk
+sudo --preserve-env=ALIS_DEPLOY_HOSTNAME,UBUNTU_VERSION "${PROGDIR}/write-image.sh" "${device}" "${file_name}"
+
+#patching distro
+sudo --preserve-env=ALIS_DEPLOY_HOSTNAME,UBUNTU_VERSION "${PROGDIR}/rpi-ubuntu-patch.sh" "${device}"
---
+++ bin/rpi-ubuntucore-get.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+readonly PROGNAME=$(basename "$0")
+readonly PROGDIR="$(dirname -- "$(readlink -f -- "$0")")"
+readonly ARGS=("$@")
+
+. "$PROGDIR"/download-functions.sh --source-only
+
+UBUNTU_VERSION=${UBUNTU_VERSION:=21.10}
+
+if [[ $# -ne 1 ]]; then
+    echo "Invalid arguments provided. Needs to receive the distro name: armhf, arm64"
+    echo "The UBUNTU_VERSION variable controls the version to download."
+    exit 1
+fi
+
+distro="$1"
+
+image_url="http://cdimage.ubuntu.com/releases/${UBUNTU_VERSION}/release/ubuntu-${UBUNTU_VERSION}-preinstalled-server-${distro}+raspi.img.xz"
+file_name="$(get_file_name_from_url "${image_url}")"
+sha256sums_url="http://cdimage.ubuntu.com/releases/${UBUNTU_VERSION}/release/SHA256SUMS"
+
+sha256sums_file_name="ubuntu-${UBUNTU_VERSION}-preinstalled-server-raspi.img.xz.sha256"
+echo ""
+download_if_newer "${image_url}"
+echo ""
+download_if_newer -O "${sha256sums_file_name}" "${sha256sums_url}"
+
+echo ""
+echo "Checking sha256 sum"
+grep "${file_name}" "${sha256sums_file_name}" | sha256sum -c
